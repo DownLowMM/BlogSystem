049d9e113ec1d4770c46504c03fd2a31e60f02e5
DuanJiaNing <aimeimeits@gmail.com>
2018-09-23 14:05:29 +0800

    登出接口

--- blogos-api/src/main/java/com/duan/blogos/api/BloggerLogoutController.java
+++ blogos-api/src/main/java/com/duan/blogos/api/BloggerLogoutController.java
@@ -1,12 +1,12 @@
 package com.duan.blogos.api;
 
+import com.duan.blogos.annonation.Uid;
 import com.duan.blogos.service.restful.ResultModel;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.duan.blogos.service.service.common.OnlineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created on 2018/2/17.
@@ -14,17 +14,15 @@ import javax.servlet.http.HttpServletRequest;
  * @author DuanJiaNing
  */
 @RestController
-@RequestMapping("/blogger/{bloggerId}/logout")
+@RequestMapping("/blogger/logout")
 public class BloggerLogoutController extends BaseCheckController {
 
+    @Autowired
+    private OnlineService onlineService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResultModel logout(HttpServletRequest request,
-                              @PathVariable Integer bloggerId) {
-        handleBloggerSignInCheck(request, bloggerId);
-        request.getSession().invalidate();
-
-        return new ResultModel<>("");
+    @GetMapping
+    public ResultModel logout(@Uid Long uid) {
+        return onlineService.logout(uid);
     }
 
 }
